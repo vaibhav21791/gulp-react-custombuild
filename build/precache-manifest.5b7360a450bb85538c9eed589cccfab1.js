cd3f2bc7b2f8cdb3ca528d5b9f3f9ec60f42533e
Vaibhav Ghorpade <vaibhav_ghorpade@live.in>
2020-05-31 15:12:48 +0530

    Add files via upload

---
+++ build/precache-manifest.5b7360a450bb85538c9eed589cccfab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34f00685914c1c4fda59ecf1c136353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f2baf45cda26440befa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "029f1973d121c4012cb5",
+    "url": "/static/js/2.e9455646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9455646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2baf45cda26440befa",
+    "url": "/static/js/main.9b185540.chunk.js"
+  },
+  {
+    "revision": "32762759838a60329c5c",
+    "url": "/static/js/runtime-main.8ac6ae30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+/**
+ * @auther Vaibhav Ghorpade
+ * @version 1.0.0.0
+ * @description gulpfile.js to Copy files from React build to CRM Webresource structure format
+ * @usage Run `gulp build` for development build. Run `gulp prod-build` for production build.
+ */
+
+var gulp = require("gulp");
+var del = require("del");
+var gulpif = require("gulp-if"); // npm i gulp-if
+var rename = require("gulp-rename"); //npm i gulp-rename
+/** JSON object to define build path and source path. Change according to the project folder structure */
+var path = {
+  devbuild: "build", // name of the React Build folder.
+  dist: { js: "dist/scripts", css: "dist/styles" }, // name of the dist folder
+};
+/** Task to delete files from dist  folder */
+gulp.task("clean-dist", function () {
+  return del([path.dist.js, path.dist.css]);
+});
+
+/** Task to copy js files from build folder to dist folder */
+gulp.task("copy-build-dist-js", function () {
+  return gulp
+    .src("build/static/js/*.*")
+    .pipe(gulpif("2.*.chunk.js", rename("chunk.js")))
+    .pipe(gulpif("2.*.chunk.js.map", rename("chunk.map.js")))
+    .pipe(gulpif("main.*.chunk.js", rename("main.js")))
+    .pipe(gulpif("main.*.chunk.js.map", rename("main.map.js")))
+    .pipe(gulpif("runtime-main.*.js", rename("runtime.js")))
+    .pipe(gulpif("runtime-main.*.js.map", rename("runtime.map.js")))
+    .pipe(gulp.dest(path.dist.js));
+  // .pipe(gulp.dest(path.dist + "/Click2Export/ThemeApp/scripts/"));
+});
+
+/** Task to copy css files from build folder to dist folder */
+gulp.task("copy-build-dist-css", function () {
+  return gulp
+    .src("build/static/css/*.css")
+    .pipe(gulpif("2.*.chunk.css", rename("chunk.css")))
+    .pipe(gulpif("main.*.chunk.css", rename("main.css")))
+    .pipe(gulp.dest(path.dist.css));
+});
+/** production mode task */
+gulp.task(
+  "build",
+  gulp.series("clean-dist", "copy-build-dist-js", "copy-build-dist-css")
+);
